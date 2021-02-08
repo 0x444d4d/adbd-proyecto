dd0672aeb0c104d50356a302725c917ff40e063e
0x444d4d <alu0101053318@ull.edu.es>
2021-02-08 17:36:41 +0000

    Update pedido.precio

--- scripts/solo triggers/pedido_incluye_comida_before_insert.sql
+++ scripts/solo triggers/pedido_incluye_comida_before_insert.sql
@@ -1,5 +1,7 @@
 CREATE DEFINER=`usuario`@`%` TRIGGER `cinesDB`.`pedido_incluye_comida_BEFORE_INSERT` BEFORE INSERT ON `pedido_incluye_comida` FOR EACH ROW
 BEGIN
+	declare curr_price int;
+  declare prod_price int;
 	DECLARE EXIT HANDLER FOR SQLEXCEPTION
         RESIGNAL;
     DECLARE EXIT HANDLER FOR SQLWARNING
@@ -9,5 +11,9 @@ BEGIN
 	call procedure_reducir_stock_cafeteria(new.idcomida, new.cantidad, new.cafeteria);
   if not exists(select pedido.id from pedido where pedido.id = new.idpedido) then
 		SIGNAL SQLSTATE '45001' SET MESSAGE_TEXT = 'No existe el pedido, crea el pedido antes de añadir items';
+	else
+		set curr_price = (select pedido.precio from pedido where pedido.id = new.idpedido);
+		set prod_price = (select comida.precio from comida where comida.id = new.idcomida);
+		update pedido set pedido.precio = (curr_price + prod_price) where pedido.id = new.idpedido;
 	end if;
 end
