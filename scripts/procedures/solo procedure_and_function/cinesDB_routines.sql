45643d7fbba83d14623f51e4091fc1a34c3bc1d7
0x444d4d <alu0101053318@ull.edu.es>
2021-02-08 21:06:57 +0000

    Update scripts

---
+++ scripts/procedures/solo procedure_and_function/cinesDB_routines.sql
@@ -0,0 +1,152 @@
+CREATE DATABASE  IF NOT EXISTS `cinesDB` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `cinesDB`;
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+--
+-- Host: localhost    Database: cinesDB
+-- ------------------------------------------------------
+-- Server version	5.7.33-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping events for database 'cinesDB'
+--
+
+--
+-- Dumping routines for database 'cinesDB'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `butacas_libres` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`usuario`@`%` PROCEDURE `butacas_libres`(fecha datetime, sala int(11), cine varchar(45), butaca int(11))
+BEGIN
+	
+    declare butacas varchar(50);
+	select a.numero from butaca as a
+			where a.numero_sala = sala not in
+			(
+				select b.butaca from compra_persona_butaca as b
+				where b.sala_butaca = sala and
+				b.cine_butaca = cine and
+				b.hora_sesion = fecha
+			
+		);
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `procedure_butacas_libres` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`usuario`@`%` PROCEDURE `procedure_butacas_libres`(fecha datetime, sala int(11), cine varchar(45), butaca int(11))
+BEGIN
+    declare butacas varchar(50);
+	select a.numero 
+    from butaca as a
+	where a.nombre_cine_sala = cine and
+	a.numero_sala = sala and
+    a.numero not in
+			(
+				select b.butaca from compra_persona_butaca as b
+				where b.sala_butaca = sala and
+				b.cine_butaca = cine and
+				b.hora_sesion = fecha
+			); 
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `procedure_reducir_stock` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`usuario`@`%` PROCEDURE `procedure_reducir_stock`(in producto int(11), in candidad int(11))
+BEGIN
+	declare cur_qtty int(11);
+    set cur_qtty = (select comida.stock from comida where comida.id = producto);
+    if (cur_qtty - cantidad) >= 0 then
+        update comida set comida.stock = (cur_qtty - cantidad) where comida.id = producto;
+	else
+		SIGNAL SQLSTATE '45001' SET MESSAGE_TEXT = 'Error con el stock';
+    end if;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `procedure_reducir_stock_cafeteria` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`usuario`@`%` PROCEDURE `procedure_reducir_stock_cafeteria`(in producto int(11), in cantidad int(11), in cafeteria int(11))
+BEGIN
+	declare cur_qtty int(11);
+    set cur_qtty = (select c.cantidad from cafeteria_oferta_comida as c
+					where c.idcomida = producto and c.idcafeteria = cafeteria);
+    
+    if (cur_qtty - cantidad) >= 0 then
+        update cafeteria_oferta_comida as c
+        set c.cantidad = (cur_qtty - cantidad)
+        where c.idcomida = producto;
+	
+    else
+		SIGNAL SQLSTATE '45001' SET MESSAGE_TEXT = 'Error con el stock';
+    
+    end if;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-02-08 20:56:55
--- scripts/triggers/solo triggers/pedido_incluye_comida_BEFORE_INSERT.sql
+++ scripts/triggers/solo triggers/pedido_incluye_comida_BEFORE_INSERT.sql
@@ -1,7 +1,7 @@
 CREATE DEFINER=`usuario`@`%` TRIGGER `cinesDB`.`pedido_incluye_comida_BEFORE_INSERT` BEFORE INSERT ON `pedido_incluye_comida` FOR EACH ROW
 BEGIN
 	declare curr_price int;
-  declare prod_price int;
+    declare prod_price int;
 	DECLARE EXIT HANDLER FOR SQLEXCEPTION
         RESIGNAL;
     DECLARE EXIT HANDLER FOR SQLWARNING
@@ -9,11 +9,12 @@ BEGIN
     DECLARE EXIT HANDLER FOR NOT FOUND
         RESIGNAL; 
 	call procedure_reducir_stock_cafeteria(new.idcomida, new.cantidad, new.cafeteria);
-  if not exists(select pedido.id from pedido where pedido.id = new.idpedido) then
+    if not exists(select pedido.id from Pedido as pedido where pedido.id = new.idpedido) then
 		SIGNAL SQLSTATE '45001' SET MESSAGE_TEXT = 'No existe el pedido, crea el pedido antes de añadir items';
-	else
-		set curr_price = (select pedido.precio from pedido where pedido.id = new.idpedido);
-		set prod_price = (select comida.precio from comida where comida.id = new.idcomida);
-		update pedido set pedido.precio = (curr_price + prod_price) where pedido.id = new.idpedido;
-	end if;
+    else
+		set curr_price = (select pedido.precio from Pedido as pedido where pedido.id = new.idpedido);
+        set prod_price = (select comida.precio from comida where comida.id = new.idcomida);
+        update Pedido set Pedido.precio = (curr_price + prod_price) where Pedido.id = new.idpedido;
+        
+    end if;
 end
